257bba7831213d0f0fd915fcd4779dc568d99c4d
Francislainy <francislainy.campos@gmail.com>
2016-12-27 11:48:36 +0000

    Added toast message to show up differently according to the pressed listview item

--- app/src/main/java/com/example/android/sampleadapter/MainActivity.java
+++ app/src/main/java/com/example/android/sampleadapter/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.sampleadapter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +16,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String[] defaultWords = new String[]{"one", "two", "three", "four", "five"};
+        String[] portugueseWords = new String[]{"um", "dois", "tres", "quatro", "cinco"};
 
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, defaultWords);
         ListView listView = (ListView) findViewById(R.id.list_item);
         listView.setAdapter(itemsAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String word = String.valueOf(parent.getItemAtPosition(position));
+                Toast.makeText(MainActivity.this, word, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
